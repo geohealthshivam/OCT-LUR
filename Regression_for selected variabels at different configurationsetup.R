9690748ca8e3fc61f1454fe98dee4fa572bfef9c
Shivam Gupta <geohealthshivam@users.noreply.github.com>
2018-09-05 21:51:34 +0200

    Proper commenting for understanding

--- Regression_for selected variabels at different configurationsetup.R
+++ Regression_for selected variabels at different configurationsetup.R
@@ -1,3 +1,4 @@
+#Linear regression using different monitoring station configuration
 lm_monitor_1<-lm(annual~buildcount_125+rdcount_1000+rdcount_500+rdlength_500+mjrdlength_500+mjrdlength_250+minrdlength_250+congestion_mjrd,monitor_1)
 
 summary(lm_monitor_1)
@@ -13,12 +14,10 @@ summary(lm_monitor_3)
 lm_monitor_4<-lm(annual~buildcount_125+rdcount_1000+rdcount_500+rdlength_500+mjrdlength_500+mjrdlength_250+minrdlength_250+congestion_mjrd,monitor_4)
 
 summary(lm_monitor_2)
-
 summary(model_lm)
 plot(shape_mun)
 plot(buildinggrid)
 plot(monitor_1,add=TRUE,pch=1)
 plot(monitor_2,add=T,pch=2)
- plot(monitor_3,add=T,pch=3)
+plot(monitor_3,add=T,pch=3)
 plot(monitor_4,add=T,pch=4)
-n
